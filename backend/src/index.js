f4777be43746de7192b9a6a3d3db87835fb62362
Kkadi20 <k.koita@alustudent.com>
2025-11-27 22:24:18 +0200

    dash board error solving

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "https://life-ease-3vcd9czsy-kadis-projects-510ce178.vercel.app"
+    ],
+    credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
